ea2f8b8b4b5b556fa2f9b185be20c4fcb04dd711
mateo marin <teosmarin@hotmail.com>
2018-09-25 01:03:37 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/MavenDemo/PruebaMaven2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.MavenDemo;
+import com.ulatina.DemoMaven.App;
+/**
+ *
+ * @author Mateo Marin
+ */
+public class PruebaMaven2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        App app = new App();
+        app.imprimir();
+    }
+    
+}
